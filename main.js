d12b9c3c7c41278be9225b8a3c0d7143132be956
Medhat <medoelking58@gmail.com>
2022-02-27 01:40:22 +0200

    version one

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-var currentURL = window.location.href;
- if (currentURL.includes("medium.com")){
-     window.location.assign(`https://mirror-medium.com/?m=${currentURL}`);
- }
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    var activeTab = tabs[0];
+    // var pendingTabURL = activeTab.pendingUrl;
+    var activeTabURL = activeTab.url;
+
+    document.getElementById("text").innerHTML = activeTabURL;
+    
+    if (activeTabURL.includes("https://medium.com/")){
+        chrome.tabs.update({
+            url: `https://mirror-medium.com/?m=${activeTabURL}`,
+        }
+        )
+        
+    }
+});
