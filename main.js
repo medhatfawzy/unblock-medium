79f0a361f3ba508c0d588221aae9485236dee0db
Medhat <medoelking58@gmail.com>
2022-02-26 23:57:36 +0200

    fixes

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-document.getElementsByClassName("text").innerText = `The Full URL of this page is ${window.location.href}`;
+document.getElementById("text").innerText = `The Full URL of this page is ${window.location.href}`;
 // window.location.assign("https://mirror-medium.com/");
